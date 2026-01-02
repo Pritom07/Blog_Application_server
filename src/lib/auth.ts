12ffa0817a9dd635136dd4d955f2559204e0e5d1
Pritom07 <pritommohajon4545@gmail.com>
2026-01-02 20:46:17 +0600

    google signup through betterAuth done

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -192,4 +192,13 @@ export const auth = betterAuth({
       }
     },
   },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      accessType: "offline",
+      prompt: "select_account consent",
+    },
+  },
 });
